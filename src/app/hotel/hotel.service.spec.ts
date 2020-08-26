69c375774467006ceda5a4958d15377ba021ef00
Maciej Kowalski <maciej.kowalski@codurance.com>
2020-08-26 11:05:42 +0100

    sdfsd

--- src/app/hotel/hotel.service.spec.ts
+++ src/app/hotel/hotel.service.spec.ts
@@ -1,6 +1,7 @@
 import { TestBed } from '@angular/core/testing';
 
 import { HotelService } from './hotel.service';
+import {HotelExistsException} from '../exceptions/hotelExistsException';
 
 describe('HotelService', () => {
   let hotelService: HotelService;
@@ -23,5 +24,17 @@ describe('HotelService', () => {
     hotelService.addHotel(hotelId, hotelName);
     // then
     expect(hotelService.findHotelById(hotelId)).not.toBe(null);
+  });
+
+  it('should not add a hotel', () => {
+    // given
+    const hotelId = 1;
+    const hotelName = 'Marriott - London';
+    hotelService.addHotel(hotelId, hotelName);
+
+    // when
+    // then
+    expect(() => hotelService.addHotel(hotelId, hotelName))
+      .toThrow(HotelExistsException);
   });
 });
