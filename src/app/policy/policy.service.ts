b895f43adeaaebd21ab24520b6cef419a52d4683
Tom <thomas.spencer@codurance.com>
2020-08-26 11:05:56 +0100

    Add policy file

---
+++ src/app/policy/policy.service.ts
@@ -0,0 +1,16 @@
+import { Injectable } from '@angular/core';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class PolicyService {
+
+  constructor() { }
+
+  setCompany(companyId: number, roomTypes: string[]): void {
+  }
+
+  setEmployee(employeeId: number, employeeRoomTypes: string[]): void {
+
+  }
+}
